abd2071548ac3c058c50983ef42dc140e7d15fad
acaly <wuzhenwei35@gmail.com>
2014-02-26 17:54:01 +0800

    Add values returned in Json

--- queryprinter/PlaceJsonPrinter.php
+++ queryprinter/PlaceJsonPrinter.php
@@ -21,4 +21,57 @@ class PlaceJSONPrinter extends JsonResultPrinter {
 
     return $definitions;
   }
+  protected function getResultText( SMWQueryResult $res, $outputmode ) {
+
+    if ( $outputmode == SMW_OUTPUT_FILE ) {
+
+      // No results, just bailout
+      if ( $res->getCount() == 0 ){
+        return $this->params['default'] !== '' ? $this->params['default'] : '';
+      }
+
+      // Serialize queryResult
+      $result = FormatJSON::encode(
+        $this->filterJson($res->serializeToArray()),
+        $this->params['prettyprint']
+      );
+
+    } else {
+      // Create a link that points to the JSON file
+      $result = $this->getLink( $res, $outputmode )->getText( $outputmode, $this->mLinker );
+
+      // Code can be viewed as HTML if requested, no more parsing needed
+      $this->isHTML = $outputmode == SMW_OUTPUT_HTML;
+    }
+
+    return $result;
+  }
+
+  protected function filterJson( $json ) {
+    $result = array(
+      'name' => '',
+      'addr' => '',
+      'baidu_lati' => '',
+      'baidu_longi' => '',
+      'google_lati' => '',
+      'google_longi' => '',
+      'email' => '',
+      'opening_hours' => '',
+      'tel' => ''
+    );
+
+    foreach ( $json['results'] as $k => $v ) {
+      $result['name'] = $k;
+      foreach ( $v['printouts'] as $vk => $vv) {
+        if ( substr($vk, 0, 5) == 'Baidu' ) {
+          $result['baidu_lati'] = $vv[0]['lat'];
+          $result['baidu_longi'] = $vv[0]['lon'];
+        } else if ( substr($vk, 0, 6) == 'Google' ) {
+          $result['google_lati'] = $vv[0]['lat'];
+          $result['google_longi'] = $vv[0]['lon'];
+        }
+      }
+    }
+    return $result;
+  }
 }
