4357fe796c6848e9739af31ee3bab1495ff173fd
acaly <wuzhenwei35@gmail.com>
2014-02-26 17:05:29 +0800

    Add Printer name

--- MAPKUDataAPI.i18n.php
+++ MAPKUDataAPI.i18n.php
@@ -3,5 +3,6 @@
 $messages = array();
 
 $messages['zh-cn'] = array(
-  'mapkudataapi-desc' => '为Semantic Mediawiki提供新的Json格式作为移动端的数据接口'
+  'mapkudataapi-desc' => '为Semantic Mediawiki提供新的Json格式作为移动端的数据接口',
+  'mapkudataapi-placejson' => '导出为针对地点的简洁JSON'
 );
--- queryprinter/PlaceJsonPrinter.php
+++ queryprinter/PlaceJsonPrinter.php
@@ -1,5 +1,14 @@
 <?php
 use SMW\JsonResultPrinter;
 class PlaceJSONPrinter extends JsonResultPrinter {
+  /**
+   * Returns human readable label for this printer
+   * @codeCoverageIgnore
+   *
+   * @return string
+   */
+  public function getName() {
+    return $this->msg( 'mapkudataapi-placejson' )->text();
+  }
 
 }
