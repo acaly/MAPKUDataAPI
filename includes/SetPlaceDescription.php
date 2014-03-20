02b56bdcf22ad8a08f64cfe859bcf3fceba6bced
acaly <wuzhenwei35@gmail.com>
2014-03-20 19:45:13 +0800

    Fix set place description hook

--- includes/SetPlaceDescription.php
+++ includes/SetPlaceDescription.php
@@ -6,14 +6,21 @@ class SetPlaceDescription {
   public static function onParserBeforeStrip( &$parser, &$text, &$strip_state ) {
     global $wgMAPKUDataAPIStr;
     
-    if (strpos($text, '!TEST API!') !== 0) return;
+    //if (strpos($text, '!TEST API!') !== 0) return;
     
     $reg = '/\{\{' .
         $wgMAPKUDataAPIStr['str_place_template_name'] .
         '[^\{\}]*(\{([^\{\}]|(?1))*\})*\}\}\n?(([^=]=?)*[^=])==/s';
 
-    $description = preg_match($reg, $text, $match, )
+    preg_match($reg, $text, $match);
 
-    $text = $text . '"' . $description . '"' . '{{#set:Is test=true}}';
+    $description = preg_replace(
+      array("/'/", "/{/", "/}/"),
+      "",
+      $match[3]
+    );
+
+    $text = $text . "\n{{#set:" . $wgMAPKUDataAPIStr['prop_place_description']
+        . '=' . $description . '}}';
   }
 }
